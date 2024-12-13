562657e92b7e2942f401ea381888a18b6d79e093
akmalkimo <akmalmorsyy@gmail.com>
2024-12-13 22:58:22 +0200

    Fix routing problem by adding basename

--- src/main.jsx
+++ src/main.jsx
@@ -6,17 +6,22 @@ import Element from './Element.jsx';
 import NotFoundPage from './NotFoundPage.jsx';
 import App from './App.jsx';
 
-const router = createBrowserRouter([
+const router = createBrowserRouter(
+  [
+    {
+      path: '/',
+      element: <App />,
+      errorElement: <NotFoundPage />
+    },
+    {
+      path: '/Element/:elementNumber',
+      element: <Element />
+    }
+  ],
   {
-    path: '/',
-    element: <App />,
-    errorElement: <NotFoundPage />
-  },
-  {
-    path: '/Element/:elementNumber',
-    element: <Element />
+      basename: '/Periodic-Table-Memorization'
   }
-]);
+);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
